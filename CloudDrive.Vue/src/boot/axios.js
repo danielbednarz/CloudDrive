bf2d1f47550e6108a6ea944a969ee828fce900cd
Szymon Baran <szymon.w.baran@gmail.com>
2022-06-06 22:24:10 +0200

    First API deployment version

--- CloudDrive.Vue/src/boot/axios.js
+++ CloudDrive.Vue/src/boot/axios.js
@@ -7,7 +7,10 @@ import axios from "axios";
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: "https://localhost:44390/api" });
+const api = axios.create({
+  //baseURL: "https://localhost:44390/api",
+  baseURL: "https://clouddriveapi.azurewebsites.net/api",
+});
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
